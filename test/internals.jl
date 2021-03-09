495f3dc4c4081ef7c5bb432bdd61c48ff7f5a5b4
chriselrod <elrodc@gmail.com>
2021-03-09 01:33:07 -0500

    Test that store sets same offset as load.

--- test/internals.jl
+++ test/internals.jl
@@ -4,11 +4,11 @@
     x = zeros(UInt, 100);
     GC.@preserve x begin
         t1 = (1.0, "hello world", 3, [1,2,3,4])
-        ThreadingUtilities.store!(pointer(x), t1, 0)
+        @test ThreadingUtilities.store!(pointer(x), t1, 0) === sizeof(t1)
         @test ThreadingUtilities.load(pointer(x), typeof(t1), 0) === (sizeof(t1), t1)
         
         t2 = (1.0, C_NULL, 3, ThreadingUtilities.stridedpointer(x))
-        ThreadingUtilities.store!(pointer(x), t2, 0)
+        @test ThreadingUtilities.store!(pointer(x), t2, 0) === mapreduce(sizeof, +, t2)
         @test ThreadingUtilities.load(pointer(x), typeof(t2), 0) === (mapreduce(sizeof, +, t2), t2)
     end
 end
