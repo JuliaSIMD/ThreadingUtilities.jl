f245550ba6dc32a2991cbd8867c235ab642e74b1
chriselrod <elrodc@gmail.com>
2021-03-09 01:32:24 -0500

    Use `sizeof` in store/load test

--- test/internals.jl
+++ test/internals.jl
@@ -5,7 +5,7 @@
     GC.@preserve x begin
         t1 = (1.0, "hello world", 3, [1,2,3,4])
         ThreadingUtilities.store!(pointer(x), t1, 0)
-        @test ThreadingUtilities.load(pointer(x), typeof(t1), 0) === (8+3sizeof(UInt), t1)
+        @test ThreadingUtilities.load(pointer(x), typeof(t1), 0) === (sizeof(t1), t1)
         
         t2 = (1.0, C_NULL, 3, ThreadingUtilities.stridedpointer(x))
         ThreadingUtilities.store!(pointer(x), t2, 0)
