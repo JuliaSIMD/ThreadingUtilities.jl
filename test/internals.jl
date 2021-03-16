b8acb216c0a993b399b916a22deeb80505afc5b4
Mal Miller <malmiller@deloitte.com.au>
2021-03-16 17:03:27 +1100

    Add general fallback store test

--- test/internals.jl
+++ test/internals.jl
@@ -6,5 +6,9 @@
         t1 = (1.0, C_NULL, 3, ThreadingUtilities.stridedpointer(x))
         @test ThreadingUtilities.store!(pointer(x), t1, 0) === mapreduce(sizeof, +, t1)
         @test ThreadingUtilities.load(pointer(x), typeof(t1), 0) === (mapreduce(sizeof, +, t1), t1)
+
+        nt1 = (;a = 1.0)
+        @test ThreadingUtilities.store!(pointer(x), nt1, 0) === sizeof(nt1)
+        @test ThreadingUtilities.load(pointer(x), typeof(nt1), 0) === (sizeof(nt1), nt1)
     end
 end
