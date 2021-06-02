0509594f5dec37a1c57225e3b5ec73ceec6357b1
chriselrod <elrodc@gmail.com>
2021-06-02 04:07:40 -0400

    More coverage.

--- test/internals.jl
+++ test/internals.jl
@@ -3,9 +3,11 @@
     @test ThreadingUtilities.store!(pointer(UInt[]), nothing, 1) == 1
     x = zeros(UInt, 100);
     GC.@preserve x begin
-        t1 = (1.0, C_NULL, (3, UInt(17)), VectorizationBase.stridedpointer(x))
+        t1 = (1.0, C_NULL, Val(7), (3, UInt(17)), VectorizationBase.stridedpointer(x))
         @test ThreadingUtilities.store!(pointer(x), t1, 0) === mapreduce(sizeof, +, t1)
+        @test ThreadingUtilities.store!(pointer(x), Val(0), 0) == 0
         @test ThreadingUtilities.load(pointer(x), typeof(t1), 0) === (mapreduce(sizeof, +, t1), t1)
+        @test ThreadingUtilities.load(pointer(x), Val{0}, 0) === (0, Val(0))
         @test ThreadingUtilities.store!(pointer(x), 0xb502916f%UInt, 72) == 72 + sizeof(Int)
         @test ThreadingUtilities.load(pointer(x), UInt, 72) == (72 + sizeof(Int),0xb502916f%UInt)
         nt1 = (;a = 1.0)
