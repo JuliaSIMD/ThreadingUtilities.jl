7eb0487635f77d99c68ff938ba3ae646bcbf3d06
chriselrod <elrodc@gmail.com>
2021-06-02 04:02:14 -0400

    Fix tests

--- test/internals.jl
+++ test/internals.jl
@@ -6,8 +6,8 @@
         t1 = (1.0, C_NULL, (3, UInt(17)), VectorizationBase.stridedpointer(x))
         @test ThreadingUtilities.store!(pointer(x), t1, 0) === mapreduce(sizeof, +, t1)
         @test ThreadingUtilities.load(pointer(x), typeof(t1), 0) === (mapreduce(sizeof, +, t1), t1)
-        @test ThreadingUtilities.store!(pointer(x), 0xb502916f%UInt, 76) == 80
-        @test ThreadingUtilities.load(pointer(x), UInt, 76) == (80,0xb502916f%UInt)
+        @test ThreadingUtilities.store!(pointer(x), 0xb502916f%UInt, 72) == 72 + sizeof(Int)
+        @test ThreadingUtilities.load(pointer(x), UInt, 72) == (72 + sizeof(Int),0xb502916f%UInt)
         nt1 = (;a = 1.0)
         @test ThreadingUtilities.store!(pointer(x), nt1, 0) === sizeof(nt1)
         @test ThreadingUtilities.load(pointer(x), typeof(nt1), 0) === (sizeof(nt1), nt1)
