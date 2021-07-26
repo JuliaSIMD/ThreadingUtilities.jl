501547d10c75ace60b451b6246faa3cfcc649836
Chris Elrod <elrodc@gmail.com>
2021-07-26 02:26:26 -0400

    Tests pass locally on 32 bit (they didn't before, even though CI apparently does?

--- src/ThreadingUtilities.jl
+++ src/ThreadingUtilities.jl
@@ -37,17 +37,28 @@ function initialize_task(tid)
   # set to tid, we have tasks 2...nthread, from 1-based ind perspective
   ccall(:jl_set_task_tid, Cvoid, (Any, Cint), t, tid % Cint)
   TASKS[tid] = t
+  return nothing
 end
 
+if Sys.WORD_SIZE == 32
+  retnothing(::Ptr{UInt}) = nothing
+end
 function __init__()
   _print_exclusivity_warning()
   nt = min(Threads.nthreads(), (Sys.CPU_THREADS)::Int) - 1
   resize!(THREADPOOL, (THREADBUFFERSIZE ÷ sizeof(UInt)) * nt + (LINESPACING ÷ sizeof(UInt)) - 1)
   copyto!(THREADPOOL, zero(UInt))
   # align to LINESPACING boundary, and then subtract THREADBUFFERSIZE to make the pointer 1-indexed
-  THREADPOOLPTR[] = reinterpret(Ptr{UInt}, (reinterpret(UInt, (pointer(THREADPOOL)))+LINESPACING-1) & (-LINESPACING)) - THREADBUFFERSIZE
+  THREADPOOLPTR[] = reinterpret(Ptr{UInt}, (reinterpret(UInt, pointer(THREADPOOL))+LINESPACING-1) & (-LINESPACING)) - THREADBUFFERSIZE
   resize!(TASKS, nt)
   foreach(initialize_task, 1:nt)
+  if Sys.WORD_SIZE == 32 && nt > 0
+    fptr = @cfunction(retnothing, Cvoid, (Ptr{UInt},))
+    store!(taskpointer(1), fptr, sizeof(UInt))
+    _atomic_xchg!(taskpointer(1), TASK)
+    wake_thread!(1)
+    wait(1)
+  end
 end
 
 end # module
