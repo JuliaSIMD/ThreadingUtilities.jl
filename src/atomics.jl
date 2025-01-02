6e19359d1eb44ed98c90c9283098a82bbdf05dff
Chengyu HAN <cyhan.dev@outlook.com>
2025-01-02 18:38:18 +0800

    atomics: update llvmcall for `load,store,cmpxchg`

--- src/atomics.jl
+++ src/atomics.jl
@@ -1,23 +1,21 @@
+
 for (ityp,jtyp) ∈ [("i8", UInt8), ("i16", UInt16), ("i32", UInt32), ("i64", UInt64), ("i128", UInt128)]
     @eval begin
         @inline function _atomic_load(ptr::Ptr{$jtyp})
             Base.llvmcall($("""
-              %p = inttoptr i$(8sizeof(Int)) %0 to $(ityp)*
-              %v = load atomic $(ityp), $(ityp)* %p acquire, align $(Base.gc_alignment(jtyp))
+              %v = load atomic $(ityp), ptr %0 acquire, align $(Base.gc_alignment(jtyp))
               ret $(ityp) %v
             """), $jtyp, Tuple{Ptr{$jtyp}}, ptr)
         end
         @inline function _atomic_store!(ptr::Ptr{$jtyp}, x::$jtyp)
             Base.llvmcall($("""
-              %p = inttoptr i$(8sizeof(Int)) %0 to $(ityp)*
-              store atomic $(ityp) %1, $(ityp)* %p release, align $(Base.gc_alignment(jtyp))
+              store atomic $(ityp) %1, ptr %0 release, align $(Base.gc_alignment(jtyp))
               ret void
             """), Cvoid, Tuple{Ptr{$jtyp}, $jtyp}, ptr, x)
         end
         @inline function _atomic_cas_cmp!(ptr::Ptr{$jtyp}, cmp::$jtyp, newval::$jtyp)
             Base.llvmcall($("""
-              %p = inttoptr i$(8sizeof(Int)) %0 to $(ityp)*
-              %c = cmpxchg $(ityp)* %p, $(ityp) %1, $(ityp) %2 acq_rel acquire
+              %c = cmpxchg ptr %0, $(ityp) %1, $(ityp) %2 acq_rel acquire
               %bit = extractvalue { $ityp, i1 } %c, 1
               %bool = zext i1 %bit to i8
               ret i8 %bool
