349c2a589ca30923b0b7f5f1d8f5e86b343ba446
Chengyu HAN <cyhan.dev@outlook.com>
2025-06-08 16:40:31 +0800

    atomics: fix compat with julia <= v1.11

--- src/atomics.jl
+++ src/atomics.jl
@@ -2,24 +2,57 @@
 for (ityp,jtyp) ∈ [("i8", UInt8), ("i16", UInt16), ("i32", UInt32), ("i64", UInt64), ("i128", UInt128)]
     @eval begin
         @inline function _atomic_load(ptr::Ptr{$jtyp})
-            Base.llvmcall($("""
-              %v = load atomic $(ityp), ptr %0 acquire, align $(Base.gc_alignment(jtyp))
-              ret $(ityp) %v
-            """), $jtyp, Tuple{Ptr{$jtyp}}, ptr)
+            Base.llvmcall($(
+                @static if VERSION >= v"1.12-DEV"
+                    # use opaque pointers #53
+                    """
+                    %v = load atomic $(ityp), ptr %0 acquire, align $(Base.gc_alignment(jtyp))
+                    ret $(ityp) %v
+                    """
+                else
+                    """
+                    %p = inttoptr i$(8sizeof(Int)) %0 to $(ityp)*
+                    %v = load atomic $(ityp), $(ityp)* %p acquire, align $(Base.gc_alignment(jtyp))
+                    ret $(ityp) %v
+                    """
+                end
+            ), $jtyp, Tuple{Ptr{$jtyp}}, ptr)
         end
         @inline function _atomic_store!(ptr::Ptr{$jtyp}, x::$jtyp)
-            Base.llvmcall($("""
-              store atomic $(ityp) %1, ptr %0 release, align $(Base.gc_alignment(jtyp))
-              ret void
-            """), Cvoid, Tuple{Ptr{$jtyp}, $jtyp}, ptr, x)
+            Base.llvmcall($(
+                @static if VERSION >= v"1.12-DEV"
+                    """
+                    store atomic $(ityp) %1, ptr %0 release, align $(Base.gc_alignment(jtyp))
+                    ret void
+                    """
+                else
+                    """
+                    %p = inttoptr i$(8sizeof(Int)) %0 to $(ityp)*
+                    store atomic $(ityp) %1, $(ityp)* %p release, align $(Base.gc_alignment(jtyp))
+                    ret void
+                    """
+                end
+            ), Cvoid, Tuple{Ptr{$jtyp}, $jtyp}, ptr, x)
         end
         @inline function _atomic_cas_cmp!(ptr::Ptr{$jtyp}, cmp::$jtyp, newval::$jtyp)
-            Base.llvmcall($("""
-              %c = cmpxchg ptr %0, $(ityp) %1, $(ityp) %2 acq_rel acquire
-              %bit = extractvalue { $ityp, i1 } %c, 1
-              %bool = zext i1 %bit to i8
-              ret i8 %bool
-            """), Bool, Tuple{Ptr{$jtyp}, $jtyp, $jtyp}, ptr, cmp, newval)
+            Base.llvmcall($(
+                @static if VERSION >= v"1.12-DEV"
+                    """
+                    %c = cmpxchg ptr %0, $(ityp) %1, $(ityp) %2 acq_rel acquire
+                    %bit = extractvalue { $ityp, i1 } %c, 1
+                    %bool = zext i1 %bit to i8
+                    ret i8 %bool
+                    """
+                else
+                    """
+                    %p = inttoptr i$(8sizeof(Int)) %0 to $(ityp)*
+                    %c = cmpxchg $(ityp)* %p, $(ityp) %1, $(ityp) %2 acq_rel acquire
+                    %bit = extractvalue { $ityp, i1 } %c, 1
+                    %bool = zext i1 %bit to i8
+                    ret i8 %bool
+                    """
+                end
+            ), Bool, Tuple{Ptr{$jtyp}, $jtyp, $jtyp}, ptr, cmp, newval)
         end
     end
 end
@@ -43,10 +76,20 @@ for op ∈ atomicrmw_ops
         @eval begin
             # Do inplace `$(op)` for `*ptr` and `x` atomically, return the old value of `*ptr`.
             @inline function $f(ptr::Ptr{$jtyp}, x::$jtyp)
-                Base.llvmcall($("""
-                  %v = atomicrmw $(op) ptr %0, $(ityp) %1 acq_rel
-                  ret $(ityp) %v
-                """), $jtyp, Tuple{Ptr{$jtyp}, $jtyp}, ptr, x)
+                Base.llvmcall($(
+                    @static if VERSION >= v"1.12-DEV"
+                        """
+                        %v = atomicrmw $(op) ptr %0, $(ityp) %1 acq_rel
+                        ret $(ityp) %v
+                        """
+                    else
+                        """
+                        %p = inttoptr i$(8sizeof(Int)) %0 to $(ityp)*
+                        %v = atomicrmw $op $(ityp)* %p, $(ityp) %1 acq_rel
+                        ret $(ityp) %v
+                        """
+                    end
+                ), $jtyp, Tuple{Ptr{$jtyp}, $jtyp}, ptr, x)
             end
         end
     end
